277cff8ac6e7cf7fd017f9246f2dab14105fa745
sanjeet <sanjeet_kumar@projects.iitmandi.ac.in>
2019-11-20 12:06:50 +0530

    Add new Sanjeet.java File

---
+++ Old_Version/Sanjeet.java
@@ -0,0 +1,4 @@
+ This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
